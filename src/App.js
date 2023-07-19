86f01b4bfc38037b9fb89b62c60983937ebdb56a
Abideen <misterjrshittu@gmail.com>
2023-07-19 13:17:46 +0100

    pixisland completed

--- src/App.js
+++ src/App.js
@@ -5,23 +5,44 @@ import SearchIcon from "./search.svg";
 import PicsComponent from './pics';
 
 const API_KEY = "M16Lq6_PG8dyMKB-2iGV7-e4lMIpv8dWU46Wn36GVwY";
-const API_URL = `https://api.unsplash.com/search/photos?page=1&query=office&client_id=${API_KEY}`;
 
 
 function App() {
 
   const [photos, setPhotos] = useState([]);
+  const [search, setSearchPhoto] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const searchPhotos = async (title) => {
+
+  const fetchPhotos = async (title, page) => {
+    const API_URL = `https://api.unsplash.com/search/photos?page=${page}&query=${search}&client_id=${API_KEY}`;
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setPhotos(data.results);
   };
 
-  useEffect(()=>{
-    searchPhotos('love');
-  }, []);
+  const searchPhotos = (title) => {
+    setCurrentPage(1);
+    fetchPhotos(title, 1);
+  };
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+    fetchPhotos(search, currentPage + 1);
+  };
+
+const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+      fetchPhotos(search, currentPage - 1);
+    }
+  };
+
+  useEffect(() => {
+    fetchPhotos('blue sky', currentPage);
+  }, [currentPage]);
+
 
   return (
     <div className="app">
@@ -29,10 +50,12 @@ function App() {
 
       <div className="search">
         <input placeholder='Search for your favorite pictures' 
-        value="love"
-        onChange={ ()=> {}}/>
+        value={search}
+        onChange={ (e)=> setSearchPhoto(e.target.value)}/>
 
-        <img src={SearchIcon} alt="search"/>
+        <img src={SearchIcon} alt="search" 
+         onClick={() => searchPhotos(search)}
+        />
 
       </div>
       {
@@ -51,6 +74,10 @@ function App() {
             </div>
         )
       } 
+      <div className='pagination'>
+        <button onClick={prevPage} disabled={currentPage === 1}>Previous</button>
+        <button onClick={nextPage}>Next</button>
+      </div>
     
     </div>
   );
